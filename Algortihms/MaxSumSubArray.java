a3504991277f6d0c153fa4ee6677e942874228dc
ShadowForgelab <waigaonkaralok@gmail.com>
2025-05-11 22:50:09 +0530

    Added prefix sum array

--- Algortihms/MaxSumSubArray.java
+++ Algortihms/MaxSumSubArray.java
@@ -21,12 +21,33 @@ public class MaxSumSubArray {
         System.out.println("Maximum sum:"+maxsum);
     }
 
+    public static void prefixMaxSumSubArray(int [] array){
+        int maxSum=0;
+        int currSum=0;
+        int [] prefix = new int[array.length];
+        prefix[0] = array[0];
+        for(int i=1;i<array.length;i++){
+            prefix[i]=prefix[i-1]+array[i];
+        }
+
+        for (int i=0;i<array.length;i++){
+            int start=i;
+            for(int j=0;j<array.length;j++){
+                int end=j;
+                currSum = start ==0? prefix[end]: prefix[end]-prefix[start-1];
+                if(maxSum<currSum){
+                    maxSum=currSum;
+                }
+            }
+        }
+        System.out.println(maxSum);
+    }
 
 
 
     public static void main(String args[]){
         int [] array = {-2,3,4,5,56};
-        maxSumSubArray(array);
+        prefixMaxSumSubArray(array);
     }
 
 }
